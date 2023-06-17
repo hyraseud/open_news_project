390454833340d00cd7ca0eae428fd370d93f8fc8
reyna moon <sdua@purdue.edu>
2023-06-17 01:57:21 -0400

    changes to App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,26 @@
 import React from "react";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Register from "./components/Register";
 import Login from "./components/Login";
 import Home from "./components/Home";
 import Replies from "./components/Replies";
 import BreakingNewsPage from "./components/BreakingNewsPage";
+
 const App = () => {
-  return (
-      <div>
-        <BrowserRouter>
-          <Routes>
-            <Route path='/' element={<Home />} />
-            <Route path='/register' element={<Register />} />
-            <Route path='/dashboard' element={<Home />} />
-            <Route path='/:id/replies' element={<Replies />} />
-              <Route exact path="/" component={Home} />
-              <Route path="/breaking-news" component={BreakingNewsPage} />
-          </Routes>
-        </BrowserRouter>
-      </div>
-  );
+    return (
+        <div>
+            <Router>
+                <Routes>
+                    <Route path="/" element={<Home />} />
+                    <Route path="/register" element={<Register />} />
+                    <Route path="/login" element={<Login />} />
+                    <Route path="/dashboard" element={<Home />} />
+                    <Route path="/:id/replies" element={<Replies />} />
+                    <Route path="/breaking-news" element={<BreakingNewsPage />} />
+                </Routes>
+            </Router>
+        </div>
+    );
 };
 
 export default App;
